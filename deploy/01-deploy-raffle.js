651b7e4927650111cc4ab32cd8b15b60cbf6e71a
rabobahago <rabobahago@gmail.com>
2023-01-08 13:03:20 +0100

    verify file added

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,20 +1,43 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
-
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address
+    let vrfCoordinatorV2Address, subscriptionId
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract(
             "VRFCoordinatorV2Mock"
         )
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        //subscript to event from code
+        const transactionResponse =
+            await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+        //fund the our subscription
+        await vrfCoordinatorV2Mock.fundSubscription(
+            subscriptionId,
+            VRF_SUB_FUND_AMOUNT
+        )
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
-    const args = []
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+    const args = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ]
+
     const raffle = await deployer("Raffle", {
         from: deployer,
         args: args,
